84d5f4d41f1d712db97e574bf797b1c65db42354
JIMINJI1 <mj013031@gmail.com>
2024-11-17 20:57:35 +0900

    feat: 사용자 인증 정보 관련 UserDetailsServiceImpl 및 UserDetailsImpl 추가

---
+++ src/main/java/org/example/expert/sequrity/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package org.example.expert.sequrity;
+
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.user.entity.User;
+import org.example.expert.domain.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자 찾을 수 없습니다"));
+        return new UserDetailsImpl(user);
+    }
+}
